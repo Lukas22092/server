263aebaf42c42e348a7f06773de4a89b2a4fdd23
Lukas <lukasheim9@icloud.com>
2026-02-05 19:57:41 +0100

    works

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,4 @@
-/*
+
 #include "foo.hpp"
 #include <termios.h>
 
@@ -18,20 +18,4 @@ int main() {
     }
 
 }
-    */
-#include <iostream>
-#include <span>
-using namespace std;
-
-int main()
-{
-    int arr[] = { 1, 2, 3, 4, 5 };
-
-    // Create a span of int of array
-    span<int> span_arr(arr);
-
-    for (const auto& num : span_arr) {
-        cout << num << " ";
-    }
-    return 0;
-}
+ 
